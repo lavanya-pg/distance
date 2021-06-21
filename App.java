49623815d5c63d6ca19e903e153505101d37e618
lavanya-pg <lavanyapg8@gmail.com>
2021-06-21 16:53:24 +0530

    calcualte distance

---
+++ App.java
@@ -0,0 +1,11 @@
+package bridgelabz.distance;
+
+public class App {
+  
+public static void main(String[] args) {
+	  int x1=4, y1=6, x2=8, y2=9;
+	   double distance = (Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+	  System.out.println(distance);
+  }
+
+}
