32074df120eb6edc70e864813e05e87e435d6e6f
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 07:51:48 +0530

    adding a program to compare the string

--- compare.java
+++ compare.java
@@ -4,30 +4,13 @@ import java.util.Iterator;
 
 public class compare {
 
-	public static boolean compare (int line1, int line2, Object[] arr2, Object[] arr) {
-		int line11 = arr. length;
-		int line21 = arr2. length;
-		
-		if(line11 != line21)
-		  return false;
-		
-		for (int i = 0; i < line11; i++)
-		if(arr[1] != arr2[i])
-			return false;
-		
-		else
-		return true;
-		return false;
+	public static void main(String[] args) {
+		String str_sample = "a";
+		System.out.println("compare to 'a' b is :" + str_sample.compareTo("b"));
+		str_sample = "b";
+		System.out.println("compare to 'b' a is :" + str_sample.compareTo("a"));
+		str_sample = "c";
+		System.out.println("compare to 'b' b is:" + str_sample.compareTo("b"));
 	}
-	
-	public static <arr, arr2> void main(String[] args) {
-		int arr[]= { 3,5 };
-		int arr2[]= { 5,3 };
-		
-		if (equal (arr, arr2))
-			System.out.println("Yes");
-		else
-			System.out.println("No");
 	}		
-}
 
